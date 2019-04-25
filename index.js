2011181ff4604c6e2c7c20292b53e5cfaffdf85e
burnpiro <kemalpiro@gmail.com>
2019-04-25 20:47:58 +0100

    Implement git branch validation for given pattern

---
+++ index.js
@@ -0,0 +1,26 @@
+const branchName = require('current-git-branch');
+
+// Exit with error when no regex provided
+if (!process.argv[2]) {
+  console.error("Usage: branch-naming-check <regexp>");
+  process.exitCode = 1;
+  return;
+}
+
+try {
+  const validBranchNameRegExp = new RegExp(process.argv[2], "g");
+
+  if (validBranchNameRegExp.test(branchName())) {
+    process.exitCode = 0;
+  } else {
+    process.exitCode = 1;
+    console.error(
+      `Current Git branch name: "${branchName()}" \n` +
+      `Is not valid for given pattern: "${validBranchNameRegExp.toString()}"\n`
+    );
+  }
+} catch (error) {
+  console.error(error.message + "\n");
+  process.exitCode = 1;
+  return;
+}
